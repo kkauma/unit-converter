629fcb5c20a22d818f884a13292b0cc8a88f57b5
kkauma <kylekauma@gmail.com>
2022-05-28 16:53:43 -0700

    change input element to only allow positive numbers

--- index.js
+++ index.js
@@ -9,38 +9,37 @@ const litersNum = document.querySelector("#liters-num");
 const poundsNum = document.querySelector("#pounds-num");
 const kilosNum = document.querySelector("#kilos-num");
 
-
 // Meters to feet, vice versa
 function metersToFeet() {
-    const feet = (num * 3.281).toFixed(3); // change so it outputs to 3 decimal places
-    feetNum.textContent = feet;
+  const feet = (num * 3.281).toFixed(3); // change so it outputs to 3 decimal places
+  feetNum.textContent = feet;
 }
 
 function feetToMeters() {
-    const meters = (num / 3.281).toFixed(3);
-    metersNum.textContent = meters;
+  const meters = (num / 3.281).toFixed(3);
+  metersNum.textContent = meters;
 }
 
 // Liters to gallons, vice versa
 function gallonsToLiters() {
-    const liters = (num * 3.785).toFixed(3);
-    litersNum.textContent = liters;
+  const liters = (num * 3.785).toFixed(3);
+  litersNum.textContent = liters;
 }
 
 function litersToGallons() {
-    const gallons = (num / 3.785).toFixed(3);
-    gallonsNum.textContent = gallons;
+  const gallons = (num / 3.785).toFixed(3);
+  gallonsNum.textContent = gallons;
 }
 
 // Kilos to pounds, vice versa
 function kilosToPounds() {
-    const pounds = (num * 2.205).toFixed(3);
-    poundsNum.textContent = pounds;
+  const pounds = (num * 2.205).toFixed(3);
+  poundsNum.textContent = pounds;
 }
 
 function poundsToKilos() {
-    const kilos = (num / 2.205).toFixed(3);
-    kilosNum.textContent = kilos;
+  const kilos = (num / 2.205).toFixed(3);
+  kilosNum.textContent = kilos;
 }
 
 // Call conversion functions
